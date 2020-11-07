b0a69ec58a5a3352093d0c4e139b0cc3367246ed
Nafiz Al din <siriusal121211@gmail.com>
2020-11-07 21:46:05 +0600

    my prj 2

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.banner_slide').carousel({
+        interval: 2000
+      })
+    
+})
+$(document).ready(function () {
+  $('#fb_frnt1').on('click', function(){
+    $('#fb').toggle(100);
+  })
+
+});
